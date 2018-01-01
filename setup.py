75ae4603d86da0c86c8520872fa83bd5495f0ad5
Robert Rees <shudderfix@gmail.com>
2018-01-01 10:36:40 +0000

    Adds the package metadata

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name =  "rrees-tag-manager",
+	packages = [
+		"rrees_tag_manager"
+	],
+	version = "0.0.1",
+	description = "Standardardised tag to string processing",
+	author = "Robert Rees",
+	author_email = "rrees-pypi@shudderfix.com",
+)

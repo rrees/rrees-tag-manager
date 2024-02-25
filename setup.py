17188e23fd8aac0d800f6222b1177ef90d604b2a
Robert Rees <shudderfix@gmail.com>
2024-02-25 19:12:43 +0000

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from distutils.core import setup
 
 setup(
-	name =  "rrees-tag-manager",
-	packages = [
-		"rrees_tag_manager"
-	],
-	version = "0.0.1",
-	description = "Standardardised tag to string processing",
-	author = "Robert Rees",
-	author_email = "rrees-pypi@shudderfix.com",
+    name="rrees-tag-manager",
+    packages=["rrees_tag_manager"],
+    version="0.0.2",
+    description="Standardardised tag to string processing",
+    author="Robert Rees",
+    author_email="rrees-pypi@shudderfix.com",
 )
